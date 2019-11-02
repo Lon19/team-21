03f5fa41f71d3f5502acb550ae6241583a2e0c56
JakeUni <jakemifsud39@gmail.com>
2019-11-02 01:24:02 +0000

    added python files

---
+++ choropleth_geojson-master/example_with_args.py
@@ -0,0 +1,25 @@
+import choropleth_geojson as cg
+import pandas as pd
+import json
+import plotly.offline as offline
+
+apikey = 'pk.eyJ1IjoiamFrZTQ3NjEiLCJhIjoiY2syZ3F5NzZxMGZ2YTNobnRzaTBvNWFwOCJ9.Ul2E9BycZxI6UNuRHRTADg'
+
+df = pd.read_csv('sg_density_data.csv', index_col = 0)
+
+with open(r'sg.json') as f:
+    geojson = json.load(f)
+
+lat = 1.3521
+lon = 103.8198
+cmap = 'viridis'
+zoom = 9.8
+opacity = 0.7
+ptitle ='Singapore population density by planning area'
+ctitle = 'Population per square kilometer'
+
+sg = cg.choropleth(apikey, df, geojson, 'name')
+fig = sg.choroplot(cmap, ptitle, ctitle, lat, lon, zoom, opacity)
+
+savefile = 'sg_density.html'
+offline.plot(fig, filename = savefile, auto_open=True)
---
+++ choropleth_geojson-master/example_with_kwargs.py
@@ -0,0 +1,25 @@
+import choropleth_geojson as cg
+import pandas as pd
+import json
+import plotly.offline as offline
+
+apikey = 'pk.eyJ1IjoiamFrZTQ3NjEiLCJhIjoiY2syZ3F5NzZxMGZ2YTNobnRzaTBvNWFwOCJ9.Ul2E9BycZxI6UNuRHRTADg'
+
+df = pd.read_csv('elspot_yearly.csv', index_col = 0)
+
+with open(r'nordpool.json') as f:
+    geojson = json.load(f)
+
+cmap = 'Spectral_r'
+ptitle ='Electricity spot prices in March 2019'
+ctitle = 'DKK per megawatt'
+col_str = 'DKK'
+lat = 61.97
+lon = 19.36
+zoom = 3.2
+
+nordpool = cg.choropleth(apikey, df, geojson, 'name')
+fig = nordpool.choroplot(cmap=cmap, ptitle=ptitle, ctitle=ctitle, col_str=col_str, lat=lat, lon=lon, zoom=zoom, width=700, height=800)
+
+savefile = 'nordpool.html'
+offline.plot(fig, filename = savefile, auto_open=True)
