138bdf3722e8489db630edb9fd5e56f8fc75d1a6
Yassin Ouzzane <youzzane@gmail.com>
2019-11-02 09:58:35 +0000

    Script complete

--- choropleth_geojson-master/csvmanip.py
+++ choropleth_geojson-master/csvmanip.py
@@ -7,8 +7,12 @@ csv_f = csv.reader(f)
 f = open("please2.csv")
 second_test = csv.reader(f)
 
+f = open("please3.csv")
+combined = csv.reader(f)
+
 dictionary1824 = {}
 dictionary2529 = {}
+dictionaryComb = {}
 
 for row in csv_f:
     dictionary1824[row[0]] = row[1]
@@ -16,6 +20,8 @@ for row in csv_f:
 for row in second_test:
     dictionary2529[row[0]] = row[1]
 
+for row in combined:
+    dictionaryComb[row[0]] = row[1]
 
 with open("wards2.json") as f:
     data = json.load(f)
@@ -25,10 +31,11 @@ for item in data["features"]:
 
     item["properties"]["data1824"] = dictionary1824.get(identif)
     item["properties"]["data2529"] = dictionary2529.get(identif)
+    item["properties"]["dataComb"] = dictionaryComb.get(identif)
     
     #item["properties"]["WD13CD"] = "\""+identif+"\","+"\"data"+"\":"+"\""+str(dictionary.get(identif))+"\","
 
-with open("works.json","w") as f:
+with open("final.json","w") as f:
     json.dump(data,f)
     
 
